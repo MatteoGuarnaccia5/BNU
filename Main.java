14874b063117f2a8bb2f73517b46915d899156ec
Matteo Guarnaccia <matteo.guarnaccia@stfc.ac.uk>
2023-10-06 11:27:59 +0100

    week 1 task 3

--- Main.java
+++ Main.java
@@ -2,8 +2,14 @@ public class Main{
 
     public static void main(String []args){
 
-       //Task 2
-       String name = System.console().readLine();
-       System.out.println("Hello " + name);
+       //Task 3
+       int length = Integer.parseInt(System.console().readLine("Enter length: "));
+       int height = Integer.parseInt(System.console().readLine("Enter height: "));
+
+       int area = length * height;
+       int perimeter = (length + height) * 2;
+
+       System.out.println("The area of the rectangle is " + area);
+       System.out.println("The perimeter of the rectangle is " + perimeter);
     }
 }
