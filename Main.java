0763a3c4a4072b00e148d8140e7098c4bfeb4ac4
Matteo Guarnaccia <matteo.guarnaccia@stfc.ac.uk>
2023-10-06 11:17:34 +0100

    Week 1 Task 2

--- Main.java
+++ Main.java
@@ -2,7 +2,8 @@ public class Main{
 
     public static void main(String []args){
 
-        //Task 1
-       System.out.println("Hello, World!");
+       //Task 2
+       String name = System.console().readLine();
+       System.out.println("Hello " + name);
     }
 }
