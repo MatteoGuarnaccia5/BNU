4013fb24bdecac5ebb157abd57ac0515ab4f08ae
Matteo Guarnaccia <matteo.guarnaccia@stfc.ac.uk>
2023-10-06 12:59:45 +0100

    week 1 task 8

--- Main.java
+++ Main.java
@@ -2,11 +2,12 @@ public class Main{
 
     public static void main(String []args){
 
-       //Task 6
-       int age = Integer.parseInt(System.console().readLine("Enter your age: "));
-       
-       int yearBorn = 2023 - age;
-       System.out.println("You were born in the year: " + yearBorn);
+       //Task 8
+       int feet = Integer.parseInt(System.console().readLine("Enter number of feet: "));
+
+       System.out.println("The number of miles is " + feet/5280);
+
+
        
     }
 }
