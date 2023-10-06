87fb45fa322c80e75b7386364f51fa52a5684d8f
Matteo Guarnaccia <matteo.guarnaccia@stfc.ac.uk>
2023-10-06 11:40:43 +0100

    week 1 task 6

--- Main.java
+++ Main.java
@@ -2,13 +2,11 @@ public class Main{
 
     public static void main(String []args){
 
-       //Task 5
-       int num1 = Integer.parseInt(System.console().readLine("Enter first number: "));
-       int num2 = Integer.parseInt(System.console().readLine("Enter second number: "));
-
-      
-
-       System.out.println("The order you entered them in: " + num1 + ", " + num2);
-       System.out.println("Reversed order: " + num2 + ", "  + num1);
+       //Task 6
+       int age = Integer.parseInt(System.console().readLine("Enter your age: "));
+       
+       int yearBorn = 2023 - age;
+       System.out.println("You were born in the year: " + yearBorn);
+       
     }
 }
