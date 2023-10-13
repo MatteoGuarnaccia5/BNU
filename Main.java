dd0605372d149ee57eaa8311a6e802ccad3137b9
Matteo Guarnaccia <matteo.guarnaccia@stfc.ac.uk>
2023-10-13 10:32:48 +0100

    week 2 task 2

--- Main.java
+++ Main.java
@@ -2,17 +2,30 @@ public class Main{
 
     public static void main(String []args){
 
-       //Task 1
-       int age = Integer.parseInt(System.console().readLine("Enter your age: "));
+        String grade = System.console().readLine("Enter your grade: ");
 
-       int yearBorn = 2023 - age;
-       System.out.println("You were born in the year: " + yearBorn);
-       if (age >= 18) {
-        System.out.println("You are 18 or older");
+        if (grade.toUpperCase().equals("A")) {
+            System.out.println("The equivalent University classification is: 1st");
+        }
+        else if (grade.toUpperCase().equals("B")) {
+            System.out.println("The equivalent University classification is: 2:1");
+        }
+        else if (grade.toUpperCase().equals("C")) {
+            System.out.println("The equivalent University classification is: 2:2");
+        }
+        else if (grade.toUpperCase().equals("D")) {
+            System.out.println("The equivalent University classification is: 3rd");
+        }
+        else if (grade.toUpperCase().equals("E")) {
+           System.out.println("The equivalent University classification is: ordinary");
+        }
+        else if (grade.toUpperCase().equals("F")) {
+           System.out.println("The equivalent University classification is: fail");
+        }
+        else{
+            System.out.println("Non valid grade entered");
+        }
+
         
-       }
-
-
-       
     }
 }
