a69070bbb9e41b95d0d479948274205b410a27fc
Matteo Guarnaccia <matteo.guarnaccia@stfc.ac.uk>
2023-10-13 10:37:14 +0100

    week 2 task 4

--- Main.java
+++ Main.java
@@ -2,9 +2,9 @@ public class Main{
 
     public static void main(String []args){
 
-        //Task 3
-        for(int i=0; i<10; i++){
-            System.out.println(i);
+        //Task 4
+        for(int i=1; i<13; i++){
+            System.out.println(i + " x 7 = " + (i*7));
         }
     }
 }
