b348d6471640244bf2f659531d76859371a2b273
Matteo Guarnaccia <matteo.guarnaccia@stfc.ac.uk>
2023-10-13 10:34:44 +0100

    week 2 task 3

--- Main.java
+++ Main.java
@@ -2,30 +2,9 @@ public class Main{
 
     public static void main(String []args){
 
-        String grade = System.console().readLine("Enter your grade: ");
-
-        if (grade.toUpperCase().equals("A")) {
-            System.out.println("The equivalent University classification is: 1st");
+        //Task 3
+        for(int i=0; i<10; i++){
+            System.out.println(i);
         }
-        else if (grade.toUpperCase().equals("B")) {
-            System.out.println("The equivalent University classification is: 2:1");
-        }
-        else if (grade.toUpperCase().equals("C")) {
-            System.out.println("The equivalent University classification is: 2:2");
-        }
-        else if (grade.toUpperCase().equals("D")) {
-            System.out.println("The equivalent University classification is: 3rd");
-        }
-        else if (grade.toUpperCase().equals("E")) {
-           System.out.println("The equivalent University classification is: ordinary");
-        }
-        else if (grade.toUpperCase().equals("F")) {
-           System.out.println("The equivalent University classification is: fail");
-        }
-        else{
-            System.out.println("Non valid grade entered");
-        }
-
-        
     }
 }
