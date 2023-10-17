6f7c8fbf753a6f8ccf8232d278981c392117403e
Matteo Guarnaccia <matteo.guarnaccia@stfc.ac.uk>
2023-10-17 16:22:32 +0100

    week 2 task 8

--- Main.java
+++ Main.java
@@ -3,33 +3,11 @@ public class Main{
     public static String grade;
     public static void main(String []args){
 
-        //Task 7
-        int mark = Integer.parseInt(System.console().readLine("Enter mark: "));
-        
-
-        if (mark >= 0 && mark<20) {
-            grade = "U";
+        //Task 8
+        for(int i = 0; i<=10; i++){
+            if(i % 2 == 0){
+                System.out.println(i);
+            }
         }
-        if (mark >=20 && mark < 30) {
-            grade = "E";
-        }
-        if (mark >= 30 && mark<45) {
-            grade = "D";
-        }
-        if (mark >=45 && mark < 55) {
-            grade = "C";
-        }
-        if (mark >= 55 && mark<65) {
-            grade = "B";
-        }
-        if (mark >=65 && mark < 75) {
-            grade = "A";
-        }
-        if (mark >= 75) {
-            grade = "A*";
-        }
-        
-        
-        System.out.println(grade);
     }
 }
