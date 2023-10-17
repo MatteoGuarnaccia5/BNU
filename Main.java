0d7155f6e2b1f473112520750678dcf082785ced
Matteo Guarnaccia <matteo.guarnaccia@stfc.ac.uk>
2023-10-17 16:01:22 +0100

    week 2 task 6

--- Main.java
+++ Main.java
@@ -2,11 +2,26 @@ public class Main{
 
     public static void main(String []args){
 
-        //Task 5
-        int num = Integer.parseInt(System.console().readLine("Enter the times table you would like to see: "));
+        //Task 6
+        boolean Continue = true;
+
+        do {
+            int num = Integer.parseInt(System.console().readLine("Enter the times table you would like to see: "));
 
         for(int i=1; i<13; i++){
             System.out.println(i + " x " + num + " = " + (i*num));
         }
+        
+        String keepGoing = System.console().readLine("Do you want to keep going? (y/n)" );
+        
+        if(keepGoing.equals("n")){
+            Continue = false;
+        }
+        if(keepGoing.equals("y")){
+            Continue = true;
+        }
+        
+        } while (Continue);
+        
     }
 }
