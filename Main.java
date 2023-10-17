3502b927efd31e2fb7a9776952894cea927a1502
Matteo Guarnaccia <matteo.guarnaccia@stfc.ac.uk>
2023-10-17 16:13:21 +0100

    week 2 task 7

--- Main.java
+++ Main.java
@@ -1,27 +1,35 @@
 public class Main{
 
+    public static String grade;
     public static void main(String []args){
 
-        //Task 6
-        boolean Continue = true;
+        //Task 7
+        int mark = Integer.parseInt(System.console().readLine("Enter mark: "));
+        
 
-        do {
-            int num = Integer.parseInt(System.console().readLine("Enter the times table you would like to see: "));
-
-        for(int i=1; i<13; i++){
-            System.out.println(i + " x " + num + " = " + (i*num));
+        if (mark >= 0 && mark<20) {
+            grade = "U";
+        }
+        if (mark >=20 && mark < 30) {
+            grade = "E";
+        }
+        if (mark >= 30 && mark<45) {
+            grade = "D";
+        }
+        if (mark >=45 && mark < 55) {
+            grade = "C";
+        }
+        if (mark >= 55 && mark<65) {
+            grade = "B";
+        }
+        if (mark >=65 && mark < 75) {
+            grade = "A";
+        }
+        if (mark >= 75) {
+            grade = "A*";
         }
         
-        String keepGoing = System.console().readLine("Do you want to keep going? (y/n)" );
         
-        if(keepGoing.equals("n")){
-            Continue = false;
-        }
-        if(keepGoing.equals("y")){
-            Continue = true;
-        }
-        
-        } while (Continue);
-        
+        System.out.println(grade);
     }
 }
