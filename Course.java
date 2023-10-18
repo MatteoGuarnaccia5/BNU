f74c3b2b2f5cc86162af48d2bf265a5212ea85a2
Matteo Guarnaccia <matteo.guarnaccia@stfc.ac.uk>
2023-10-18 10:18:25 +0100

    week 3 task 7

--- Course.java
+++ Course.java
@@ -1,6 +1,15 @@
 public class Course{
 
-    //Task 6
     String courseCode;
     String name;
+
+    //Task 7
+    public Course(String code, String name){
+        this.courseCode = code;
+        this.name = name;
+    }
+
+    public void printDetails(){
+        System.out.println("Code: " + this.courseCode + "\nName: " + this.name);
+    }
 }
