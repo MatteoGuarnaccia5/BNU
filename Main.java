2b46b134a80ec87680b206b5c2579f5471329e93
Matteo Guarnaccia <matteo.guarnaccia@stfc.ac.uk>
2023-10-18 10:58:25 +0100

    week 3 task 12

--- Main.java
+++ Main.java
@@ -3,11 +3,11 @@ public class Main{
     public static void main(String[] args){
         
         Student student = new Student(1, "Matteo");
-       //Task 11
-       Course course = new Course("COM-4023", "Programming concepts");
+        Course course = new Course("COM-4023", "Programming concepts");
 
-       student.enroll(course);
-       student.printDetails();
+        student.enroll(course);
+        //Task 12
+        student.printDetails();
     }
     
 }
