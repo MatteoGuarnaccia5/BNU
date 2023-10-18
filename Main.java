19a8677e28b2d87301c823986cb89da9ef20c010
Matteo Guarnaccia <matteo.guarnaccia@stfc.ac.uk>
2023-10-18 09:38:40 +0100

    week 3 task 5

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main{
 
     public static void main(String[] args){
-        //Task 4
+        
         Student student = new Student(1, "Matteo");
-
+        //Task 5
         student.printDetails();
     }
     
