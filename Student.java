10d238f919eede527f523b6efbe71b220a12465f
Matteo Guarnaccia <matteo.guarnaccia@stfc.ac.uk>
2023-10-18 09:27:09 +0100

    week 3 task 2

--- Student.java
+++ Student.java
@@ -1,6 +1,12 @@
 public class Student {
-    
+
     //Task 1
     int id;
     String name;
+
+    //Task 2
+    public void student(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
 }
