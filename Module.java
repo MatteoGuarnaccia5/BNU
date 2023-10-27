c284c9f66e6408c1ccb2f67a87c6bb40e5e26ad0
Matteo Guarnaccia <matteo.guarnaccia@stfc.ac.uk>
2023-10-27 14:03:15 +0100

    week 4 task 9

--- Module.java
+++ Module.java
@@ -3,37 +3,48 @@ public class Module{
     String name;
     String code;
 
+    //Task 9
+    enum Grade {
+        A,
+        B,
+        C,
+        D,
+        E,
+        F,
+        U
+    }
+
     public Module(String name, String code){
 
         this.name = name;
         this.code = code;
     }
-    //Task 8
-    public String markToGrade(int mark){
+    //Task 9
+    public Grade markToGrade(int mark){
         
         if (mark <= 34) {
-            return "F";
+            return Grade.U;
         }
         else if (mark > 34 && mark <= 39){
-            return "E";
+            return Grade.F;
         }
         else if (mark > 39 && mark <= 49){
-            return "D";
+            return Grade.E;
         }
         else if (mark > 49 && mark <= 59){
-            return "C";
+            return Grade.D;
         }
         else if (mark > 59 && mark <= 69){
-            return "B";
+            return Grade.C;
         }
         else if (mark > 69 && mark <= 79){
-            return "A";
+            return Grade.B;
         }
         else if (mark > 79 && mark <= 100){
-            return "A+";
+            return Grade.A;
         }
         else{
-            return "error";
+            return Grade.U;
         }
     }
 }
