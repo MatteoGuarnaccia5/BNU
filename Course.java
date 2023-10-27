3862ddd7893d4063549032eaebe0eae2ce0bf56c
Matteo Guarnaccia <matteo.guarnaccia@stfc.ac.uk>
2023-10-27 12:13:33 +0100

    week 4 task 3

--- Course.java
+++ Course.java
@@ -5,15 +5,12 @@ public class Course{
     String courseCode;
     String name;
 
-    //Task 2
-    ArrayList<Module> Modules;
+    ArrayList<Module> Modules = new ArrayList<Module>();
 
     public Course(String code, String name){
         this.courseCode = code;
         this.name = name;
 
-        
-        //Task 2
         Modules.add(new Module("Programing Concepts", "94850948"));
         Modules.add(new Module("Work based learning", "66565566"));
         Modules.add(new Module("English", "44343423"));
@@ -21,6 +18,10 @@ public class Course{
     }
 
     public void printDetails(){
-        System.out.println("Code: " + this.courseCode + "\nName: " + this.name);
+    
+        //Task 3
+        for (int i = 0; i < Modules.size(); i++) {
+            System.out.println("The course name: " + Modules.get(i).name + "\nThe course code: " + Modules.get(i).code);
+        }
     }
 }
--- Main.java
+++ Main.java
@@ -2,7 +2,9 @@ public class Main{
 
     public static void main(String[] args){
         
-
+        //Task 3
+        Course course = new Course("909090090", "Digitech");
+        course.printDetails();
     }
     
 }
